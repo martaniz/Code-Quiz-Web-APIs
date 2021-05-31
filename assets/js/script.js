5ca7bc843fbb3d0e5380a3957dc243831c064b78
MsNiz <mniz416@gmail.com>
2021-05-31 14:35:00 -0400

    qanda

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,90 @@
 //so we have a button, now lets add the timer.
 //timer added from our exercise in week 4
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
-var startBtn = document.getElementById('save-task');
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 
-var message =
-  'Game Over';
-var words = message.split(' ');
 
-// Timer that counts down from 5
-function countdown() {
-  var timeLeft = 2;
 
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function() {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();
+//we wil need at least 2 function one to start quoiz and then view score
+function buildQuiz(){
+  // variable to store the HTML output
+  const output = [];
+
+  // for each question...
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+
+      // variable to store the list of possible answers
+      const answers = [];
+
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
     }
-  }, 1000);
+  );
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
 }
 
-// Displays the message one word at a time
-function displayMessage() {
-    var wordCount = 0;
+function showResults(){}
 
-  // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
-  var msgInterval = setInterval(function() {
-    if (words[wordCount] === undefined) {
-      clearInterval(msgInterval);
-    } else {
-      mainEl.textContent = words[wordCount];
-      wordCount++;
+myQuestions.forEach( (currentQuestion, questionNumber) => {
+  // the code we want to run for each question goes here
+});
+
+
+buildQuiz();
+
+submitButton.addEventListener('click', showResults);
+
+
+// i will use const becouse we cannot change constant primitive values, but we can change the properties of constant objects.
+const myQuestions = [
+  {
+    question: "question number one",
+    answer:{
+      a: "wrong anwser",
+      b: "wrong anwser",
+      c: "correct anwser"
+    },
+    correctAnswer: "c"
+  },
+    {
+      question: "question nubmer two",
+      anwser: {
+        a: "wrong anwser",
+        b: "wrong anwser",
+        c: "correct anwser"
+      },
+      correctAnwser: "c"
+    },
+    {
+    question: "question number 3",
+    answer: {
+      a: "wrong anwser",
+      b: "wrong anwser",
+      c: "wrong annwser",
+      d: "coredt anwser"
+    },
     }
-  }, 300);
-}
+];
 
-startBtn.onclick = countdown;
+
+
