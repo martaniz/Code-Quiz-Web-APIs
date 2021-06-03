b0e3e1902c962a625261ae07495a356a3fca2ab9
MsNiz <mniz416@gmail.com>
2021-06-03 16:47:12 -0400

    added questions, functions, starte gaem, end game etc. with help of ta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,159 @@
+// // Creating and using arrays and objects
+// // Creating and using functions
+// // Using events (eg. load event)
+// // Adding user input such as radio buttons
+// // Using for loops to repeat code
+// // Using if statements to test conditions
+
+// var pos = 0;
+// var correct = 0;
+// var test, test_status, question, choice, choices, chA, chB, chC;
+
+// // this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
+  {
+      question: "What is 36 + 42",
+      a: "64",
+      b: "78",
+      c: "76",
+      answer: "B"
+    },
+  {
+      question: "What is 7 x 4?",
+      a: "21",
+      b: "27",
+      c: "28",
+      answer: "C"
+    },
+  {
+      question: "What is 16 / 4?",
+      a: "4",
+      b: "6",
+      c: "3",
+      answer: "A"
+    },
+  {
+      question: "What is 8 x 12?",
+      a: "88",
+      b: "112",
+      c: "96",
+      answer: "C"
+    }
+  ];
+
+//   function get(x){
+//     return document.getElementById(x);
+//   }
+
+//   // this function renders a question for display on the page
+// function renderQuestion(){
+//   test = get("test");
+//   if(pos >= questions.length){
+//     test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+//     get("test_status").innerHTML = "Test completed";
+//     // resets the variable to allow users to restart the test
+//     pos = 0;
+//     correct = 0;
+//     // stops rest of renderQuestion function running when test is completed
+//     return false;
+//   }
+//   get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+  
+//   question = questions[pos].question;
+//   chA = questions[pos].a;
+//   chB = questions[pos].b;
+//   chC = questions[pos].c;
+//   // display the question
+//   test.innerHTML = "<h3>"+question+"</h3>";
+//   // display the answer options
+//   // the += appends to the data we started on the line above
+//   test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+//   test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+//   test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+//   test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+// }
+
+// function checkAnswer(){
+//   // use getElementsByName because we have an array which it will loop through
+//   choices = document.getElementsByName("choices");
+//   for(var i=0; i<choices.length; i++){
+//     if(choices[i].checked){
+//       choice = choices[i].value;
+//     }
+//   }
+//   // checks if answer matches the correct choice
+//   if(choice == questions[pos].answer){
+//     //each time there is a correct answer this value increases
+//     correct++;
+//   }
+//   // changes position of which character user is on
+//   pos++;
+//   // then the renderQuestion function runs again to go to next question
+//   renderQuestion();
+// }
+
+// // Add event listener to call renderQuestion on page load event
+// window.addEventListener("load", renderQuestion);
+
+
+//
+
 //so we have a button, now lets add the timer.
 //timer added from our exercise in week 4
+var questionNum = 0;
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('save-task');
-
-
-
-
-
-
-
-
-
-
+var answerA = document.getElementById("a")
+var question = document.getElementById("question")
+var questionBlock = document.getElementById("questions")
 
 var message =
   'Game Over';
 var words = message.split(' ');
 
+function startQuiz() {
+  document.getElementById("save-task").style.visibility ="hidden";
+  questionBlock.style.visibility="visible"
+  countdown();
+  renderQuestions();
+  // make whatever text disappear
+  // start timer
+  // render the first question and answers in the question div
+}
+
+function renderQuestions() {
+  if (questionNum >3) {
+    endQuiz();
+  } else {
+console.log(questions[questionNum])
+
+    document.getElementById("question").textContent = questions[questionNum].question
+    answerA.textContent = questions[questionNum].a
+    document.getElementById("b").textContent = questions[questionNum].b
+    document.getElementById("c").textContent = questions[questionNum].c
+    answerA.onclick = checkAnswer
+
+  }
+}
+
+function checkAnswer(){
+  console.log("answer was checked")
+  questionNum++
+  renderQuestions()
+}
+function endQuiz(){
+  questionBlock.style.visibility="hidden"
+  console.log("quize has ended");
+  }
+
+
+// another function that evaluates the question number
+// check if questionNum is > the number of questions if it is then end the quiz
+
 // Timer that counts down from 5
 function countdown() {
-  var timeLeft = 2;
+  var timeLeft = 5;
 
   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
@@ -40,10 +173,14 @@ function countdown() {
       // Use `clearInterval()` to stop the timer
       clearInterval(timeInterval);
       // Call the `displayMessage()` function
-      displayMessage();
+      // displayMessage();
+      endQuiz();
     }
   }, 1000);
 }
+
+
+
 
 // Displays the message one word at a time
 function displayMessage() {
@@ -58,29 +195,14 @@ function displayMessage() {
       wordCount++;
     }
   }, 300);
-}var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+}
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-};
 
-buttonEl.addEventListener("click", createTaskHandler);
 
-startBtn.onclick = countdown;
 
-//add click of a button, creat task handler , event lstener 
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-};
-//event listener
-buttonEl.addEventListener("click", createTaskHandler);
+
+startBtn.onclick = startQuiz;
+
+
+
